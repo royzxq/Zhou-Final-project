61d3215242e90f2b71fee2359bd22d24692a37da
royzxq <xinquanzhou@lawn-128-61-127-183.lawn.gatech.edu>
2014-03-09 00:50:38 -0500

    source code

---
+++ Source/MyProject.cpp
@@ -0,0 +1,88 @@
+/*
+  ==============================================================================
+
+    MyProject.cpp
+    Created: 8 Mar 2014 12:37:05pm
+    Author:  Xinquan Zhou
+
+  ==============================================================================
+*/
+
+#include "MyProject.h"
+
+void MultiTapDelay::setDelay(float newDelay1_sec)
+{
+    delay1_sec = newDelay1_sec;
+    delay1_sample = delay1_sec * sample_rate;
+    delay2_sample = delay1_sample * ratio1;
+    delay3_sample = delay1_sample * ratio2;
+}
+
+void MultiTapDelay::setMode(int idx)
+{
+    switch (idx) {
+        case singleTap:
+            modeSelector = 1;
+            break;
+        case multiTap:
+            modeSelector = 2;
+            break;
+        default:
+            break;
+    }
+}
+
+void MultiTapDelay::process(float **input, float **output, int iBlocksize)
+{
+    if (modeSelector == 1) {
+        for (int i = 0 ; i < iNumChannel; i++) {
+            for (int j = 0 ; j < iBlocksize; j++) {
+                pTap1[i]->putPostInc(input[i][j]);
+                pTap2[i]->putPostInc(input[i][j]);
+                pTap3[i]->putPostInc(input[i][j]);
+                output[i][j] = pTap1[i]->get(delay1_sample);
+                output[i][j] += input[i][j];
+            }
+        }
+    }
+    else if(modeSelector == 2){
+        for (int i = 0 ; i < iNumChannel; i++) {
+            for (int j = 0 ; j < iBlocksize; j++) {
+                pTap1[i]->putPostInc(input[i][j]);
+                pTap2[i]->putPostInc(input[i][j]);
+                pTap3[i]->putPostInc(input[i][j]);
+                output[i][j] = pTap1[i]->get(delay1_sample) + pTap2[i]->get(delay2_sample) + pTap3[i]->get(delay3_sample);
+                output[i][j] /= 3;
+                output[i][j] += input[i][j];
+
+            }
+        }
+    }
+}
+
+void MultiTapDelay::processBypass(float **input, float **output, int iBlocksize){
+    if (modeSelector == 1) {
+        for (int i = 0 ; i < iNumChannel; i++) {
+            for (int j = 0 ; j < iBlocksize; j++) {
+                pTap1[i]->putPostInc(input[i][j]);
+                pTap2[i]->putPostInc(input[i][j]);
+                pTap3[i]->putPostInc(input[i][j]);
+                
+                output[i][j] = input[i][j];
+            }
+        }
+    }
+    else if(modeSelector == 2){
+        for (int i = 0 ; i < iNumChannel; i++) {
+            for (int j = 0 ; j < iBlocksize; j++) {
+                pTap1[i]->putPostInc(input[i][j]);
+                pTap2[i]->putPostInc(input[i][j]);
+                pTap3[i]->putPostInc(input[i][j]);
+                output[i][j] = pTap1[i]->get(delay1_sample) + pTap2[i]->get(delay2_sample) + pTap3[i]->get(delay3_sample);
+               
+                output[i][j] = input[i][j];
+                
+            }
+        }
+    }
+}
