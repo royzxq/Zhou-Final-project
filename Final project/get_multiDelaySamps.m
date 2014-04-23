c9ca72966aaed04886b590042f9dc584486cda38
royzxq <xinquanzhou@lawn-128-61-116-122.lawn.gatech.edu>
2014-04-22 20:07:51 -0400

    Temp version

--- Final project/get_multiDelaySamps.m
+++
@@ -1,9 +0,0 @@
-function [ delay1, delay2, delay3 ] = get_multiDelaySamps( first_delay_sec, sampleRate )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-delay1 = first_delay_sec*sampleRate;
-delay2 = delay1*1.86;
-delay3 = delay2*2.72;
-end
-
---
+++ Space echo/Source/PluginProcessor.h
@@ -0,0 +1,106 @@
+/*
+ ==============================================================================
+ 
+ This file was auto-generated!
+ 
+ It contains the basic startup code for a Juce application.
+ 
+ ==============================================================================
+ */
+
+#ifndef PLUGINPROCESSOR_H_INCLUDED
+#define PLUGINPROCESSOR_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "juce_Reverb.h"
+#include "MyProject.h"
+#include "Noise.h"
+
+
+//==============================================================================
+/**
+ */
+class NewProjectAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    NewProjectAudioProcessor();
+    ~NewProjectAudioProcessor();
+    
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock);
+    void releaseResources();
+    
+    void processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages) override;
+    void processBlockBypassed(AudioSampleBuffer& buffer, MidiBuffer& midiMessages) override;
+    //==============================================================================
+    AudioProcessorEditor* createEditor();
+    bool hasEditor() const;
+    
+    //==============================================================================
+    const String getName() const;
+    
+    int getNumParameters();
+    
+    float getParameter (int index);
+    void setParameter (int index, float newValue);
+    
+    const String getParameterName (int index);
+    const String getParameterText (int index);
+    
+    const String getInputChannelName (int channelIndex) const;
+    const String getOutputChannelName (int channelIndex) const;
+    bool isInputChannelStereoPair (int index) const;
+    bool isOutputChannelStereoPair (int index) const;
+    
+    bool acceptsMidi() const;
+    bool producesMidi() const;
+    bool silenceInProducesSilenceOut() const;
+    double getTailLengthSeconds() const;
+    
+    //==============================================================================
+    int getNumPrograms();
+    int getCurrentProgram();
+    void setCurrentProgram (int index);
+    const String getProgramName (int index);
+    void changeProgramName (int index, const String& newName);
+    
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData);
+    void setStateInformation (const void* data, int sizeInBytes);
+    
+    enum Param{
+        DelayParam = 0 ,
+        ModeParam,
+        IntensityParam,
+        RepeatParam,
+        ReverbParam,
+        oldParam,
+        bypassParam,
+        FBParam,
+        RangeParam,
+        numParam
+        
+    };
+    
+    float delay_sec;
+    float intensity;
+    float repeat_rate;
+    float reverb_volumn;
+    float fb_gain;
+    float InRange;
+    bool bypass;
+    bool old;
+    int mode;
+    
+    int lastUIWidth,lastUIHeight;
+private:
+    //==============================================================================
+    MultiTapDelay * myMultiDelay;
+    WhiteNoiseGen * myNoise;
+    WavShaper   * myShaper;
+    Reverb * myReverb;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessor)
+};
+
+#endif  // PLUGINPROCESSOR_H_INCLUDED
